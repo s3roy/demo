f3ae473b929fbfaf4943da153c733281739783d6
Souvik <souvikroy1999ab@gmail.com>
2023-09-07 12:00:01 +0530

    testing

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -33,90 +33,92 @@ const Homepage = () => {
   );
 
   return (
-    <Grid
-      container
-      style={backgroundImageStyle}
-      justifyContent="flex-end"
-      alignItems="center"
-    >
-      <Paper
-        sx={{
-          width: { xs: '100vw', lg: '50vw' },
-          minHeight: { xs: '84vh', lg: '100vh' },
-          borderRadius: { xs: '40px', xl: '60px 0px 0px 60px' },
-        }}
+    <Box height="90vh">
+      <Grid
+        container
+        style={backgroundImageStyle}
+        justifyContent="flex-end"
+        alignItems="center"
       >
-        <Grid
-          display="flex"
-          justifyContent={{ xs: 'center', lg: 'end' }}
-          my={{ xs: 2, lg: 5 }}
-          mr={{ xs: 0, lg: 5 }}
+        <Paper
+          sx={{
+            width: { xs: '100vw', lg: '50vw' },
+            minHeight: { xs: '84vh', lg: '100vh' },
+            borderRadius: { xs: '40px', xl: '60px 0px 0px 60px' },
+          }}
         >
-          <Image src={Logo} alt="logo" width={50} height={50} />
-        </Grid>
-
-        <Box mx={{ xs: 2, sm: 4, md: 8, lg: 18 }}>
-          <Grid display="flex" justifyContent="center">
-            <Typography variant="h4" fontWeight="bold" component="div">
-              Teach anytime,
-              <Box
-                component="span"
-                sx={{
-                  display: { xs: 'block', lg: 'inline' },
-                  textAlign: { xs: 'center', lg: 'left' },
-                }}
-              >
-                anywhere!
-              </Box>
-            </Typography>
-          </Grid>
-
           <Grid
             display="flex"
-            justifyContent="space-between"
-            mt={{ xs: 0.5, lg: 2 }}
+            justifyContent={{ xs: 'center', lg: 'end' }}
+            my={{ xs: 2, lg: 5 }}
+            mr={{ xs: 0, lg: 5 }}
           >
-            <Stack mr={2}>
-              {renderLabel('First Name')}
-              <TextField type="text" />
-            </Stack>
-            <Stack>
-              {renderLabel('Last Name')}
-              <TextField type="text" />
-            </Stack>
+            <Image src={Logo} alt="logo" width={50} height={50} />
           </Grid>
 
-          {info.map((label) => (
-            <Grid key={label} mt={{ xs: 0.5, lg: 2 }}>
+          <Box mx={{ xs: 2, sm: 4, md: 8, lg: 18 }}>
+            <Grid display="flex" justifyContent="center">
+              <Typography variant="h4" fontWeight="bold" component="div">
+                Teach anytime,
+                <Box
+                  component="span"
+                  sx={{
+                    display: { xs: 'block', lg: 'inline' },
+                    textAlign: { xs: 'center', lg: 'left' },
+                  }}
+                >
+                  anywhere!
+                </Box>
+              </Typography>
+            </Grid>
+
+            <Grid
+              display="flex"
+              justifyContent="space-between"
+              mt={{ xs: 0.5, lg: 2 }}
+            >
+              <Stack mr={2}>
+                {renderLabel('First Name')}
+                <TextField type="text" />
+              </Stack>
               <Stack>
-                {renderLabel(label)}
-                <TextField type={label.toLowerCase()} />
+                {renderLabel('Last Name')}
+                <TextField type="text" />
               </Stack>
             </Grid>
-          ))}
 
-          <Grid mt={{ xs: 1, lg: 2 }}>
-            <Button
-              variant="contained"
-              fullWidth
-              style={{ background: 'violet' }}
-              sx={{ borderRadius: '10px' }}
+            {info.map((label) => (
+              <Grid key={label} mt={{ xs: 0.5, lg: 2 }}>
+                <Stack>
+                  {renderLabel(label)}
+                  <TextField type={label.toLowerCase()} />
+                </Stack>
+              </Grid>
+            ))}
+
+            <Grid mt={{ xs: 1, lg: 2 }}>
+              <Button
+                variant="contained"
+                fullWidth
+                style={{ background: 'violet' }}
+                sx={{ borderRadius: '10px' }}
+              >
+                Sign Up
+              </Button>
+            </Grid>
+
+            <Grid
+              display="flex"
+              justifyContent={{ xs: 'flex-start', lg: 'center' }}
+              mt={{ xs: 1, lg: 2 }}
             >
-              Sign Up
-            </Button>
-          </Grid>
-
-          <Grid
-            display="flex"
-            justifyContent={{ xs: 'flex-start', lg: 'center' }}
-            mt={{ xs: 1, lg: 2 }}
-          >
-            <Typography mr={1}>Already have an account?</Typography>
-            <Link href="#"> Login</Link>
-          </Grid>
-        </Box>
-      </Paper>
-    </Grid>
+              <Typography mr={1}>Already have an account?</Typography>
+              <Link href="#"> Login</Link>
+            </Grid>
+          </Box>
+        </Paper>
+      </Grid>
+    </Box>
   );
 };
 
