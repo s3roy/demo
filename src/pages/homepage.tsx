715ce656b8c0ca99850698d2135a1f7e8056a74b
Souvik <souvikroy1999ab@gmail.com>
2023-09-07 11:46:04 +0530

    changes

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -38,12 +38,13 @@ const Homepage = () => {
       style={backgroundImageStyle}
       justifyContent="flex-end"
       alignItems="center"
+      maxHeight="100vh"
     >
       <Paper
         sx={{
           width: { xs: '100vw', lg: '50vw' },
-          marginY: { xs: '3vh', lg: '0vh' },
-          minHeight: { xs: '84vh', lg: '100vh' },
+          marginY: { xs: '2vh', lg: '0vh' },
+          minHeight: { xs: '8vh', lg: '100vh' },
           borderRadius: { xs: '40px', xl: '60px 0px 0px 60px' },
         }}
       >
